ca56d8d0b23f615e2f05260b156da4fb55f10123
Kyle Neideck <kyle@bearisdriving.com>
2017-06-12 13:52:59 +1000

    `travis setup releases`

--- package.sh
+++ package.sh
@@ -53,6 +53,7 @@ version="$(/usr/libexec/PlistBuddy \
     -c "Print CFBundleShortVersionString" \
     "BGMApp/build/Release/Background Music.app/Contents/Info.plist")"
 
+# Everything in out_dir at the end of this script will be released in the Travis CI builds.
 out_dir="Background-Music-$version"
 rm -rf "$out_dir"
 mkdir "$out_dir"
