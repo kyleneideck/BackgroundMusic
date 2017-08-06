kyleneideck/BackgroundMusic

commit e05acde3514465e7b13125cb755b93cafaec2a3e

BGMApp: Move functions for talking to BGMDevice into their own class.
There's still some code left that should be moved into the new class,
BGMBackgroundMusicDevice, but I think this is most of it.

This also helps reduce/contain the code that has to be aware of the
second instance of BGMDevice, which handles UI-related audio.

BGMApp/BGMApp/BGMAppVolumes.mm
@@ -408,9 +408,9 @@ static CGFloat const kAppVolumeViewInitialHeight = 20;
 
     // The values from our sliders are in
     // [kAppRelativeVolumeMinRawValue, kAppRelativeVolumeMaxRawValue] already.
-    [context.audioDevices sendAppVolumeToBGMDevice:self.intValue
-                                      appProcessID:appProcessID
-                                       appBundleID:appBundleID];
+    context.audioDevices.bgmDevice.SetAppVolume(self.intValue,
+                                                appProcessID,
+                                                (__bridge CFStringRef)appBundleID);
 }
 
 @end
@@ -454,9 +454,9 @@ static CGFloat const kAppVolumeViewInitialHeight = 20;
     DebugMsg("BGMAppVolumes::appPanPositionChanged: App pan position for %s changed to %d", appBundleID.UTF8String, self.intValue);
 
     // The values from our sliders are in [kAppPanLeftRawValue, kAppPanRightRawValue] already.
-    [context.audioDevices sendAppPanPositionToBGMDevice:self.intValue
-                                           appProcessID:appProcessID
-                                            appBundleID:appBundleID];
+    context.audioDevices.bgmDevice.SetAppPanPosition(self.intValue,
+                                                     appProcessID,
+                                                     (__bridge CFStringRef)appBundleID);
 }
 
 @end

BGMApp/BGMApp/BGMBackgroundMusicDevice.cpp
@@ -1,0 +1,230 @@
+// This file is part of Background Music.
+//
+// Background Music is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 2 of the
+// License, or (at your option) any later version.
+//
+// Background Music is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Background Music. If not, see <http://www.gnu.org/licenses/>.
+
+//
+//  BGMBackgroundMusicDevice.cpp
+//  BGMApp
+//
+//  Copyright © 2016, 2017 Kyle Neideck
+//  Copyright © 2017 Andrew Tonner
+//
+
+// Self Include
+#include "BGMBackgroundMusicDevice.h"
+
+// Local Includes
+#include "BGM_Types.h"
+#include "BGM_Utils.h"
+
+// PublicUtility Includes
+#include "CADebugMacros.h"
+#include "CAHALAudioSystemObject.h"
+#include "CACFArray.h"
+#include "CACFDictionary.h"
+
+// STL Includes
+#include <map>
+
+
+#pragma clang assume_nonnull begin
+
+#pragma mark Construction/Destruction
+
+BGMBackgroundMusicDevice::BGMBackgroundMusicDevice()
+:
+    BGMAudioDevice(CFSTR(kBGMDeviceUID)),
+    mUISoundsBGMDevice(CFSTR(kBGMDeviceUID_UISounds))
+{
+    if((GetObjectID() == kAudioObjectUnknown) || (mUISoundsBGMDevice == kAudioObjectUnknown))
+    {
+        Throw(CAException(kAudioHardwareIllegalOperationError));
+    }
+};
+
+BGMBackgroundMusicDevice::~BGMBackgroundMusicDevice()
+{
+}
+
+#pragma mark Systemwide Default Device
+
+void BGMBackgroundMusicDevice::SetAsOSDefault()
+{
+    DebugMsg("BGMBackgroundMusicDevice::SetAsOSDefault: Setting the system's default audio device "
+             "to BGMDevice");
+
+    CAHALAudioSystemObject audioSystem;
+
+    AudioDeviceID defaultDevice = audioSystem.GetDefaultAudioDevice(false, false);
+    AudioDeviceID systemDefaultDevice = audioSystem.GetDefaultAudioDevice(false, true);
+
+    if(systemDefaultDevice == defaultDevice)
+    {
+        // The default system device is the same as the default device, so change both of them.
+        //
+        // Use the UI sounds instance of BGMDevice because the default system output device is the
+        // device "to use for system related sound". The allows BGMDriver to tell when the audio it
+        // receives is UI-related.
+        audioSystem.SetDefaultAudioDevice(false, true, mUISoundsBGMDevice);
+    }
+
+    audioSystem.SetDefaultAudioDevice(false, false, GetObjectID());
+}
+
+void BGMBackgroundMusicDevice::UnsetAsOSDefault(AudioDeviceID inOutputDeviceID)
+{
+    CAHALAudioSystemObject audioSystem;
+
+    // Set BGMApp's output device as OS X's default output device.
+    bool bgmDeviceIsDefault =
+            (audioSystem.GetDefaultAudioDevice(false, false) == GetObjectID());
+
+    if(bgmDeviceIsDefault)
+    {
+        DebugMsg("BGMBackgroundMusicDevice::UnsetAsOSDefault: Setting the system's default output "
+                 "device back to device %d", inOutputDeviceID);
+
+        audioSystem.SetDefaultAudioDevice(false, false, inOutputDeviceID);
+    }
+
+    // Set BGMApp's output device as OS X's default system output device.
+    bool bgmDeviceIsSystemDefault =
+            (audioSystem.GetDefaultAudioDevice(false, true) == mUISoundsBGMDevice);
+
+    // If we changed the default system output device to BGMDevice, which we only do if it's set to
+    // the same device as the default output device, change it back to the previous device.
+    if(bgmDeviceIsSystemDefault)
+    {
+        DebugMsg("BGMBackgroundMusicDevice::UnsetAsOSDefault: Setting the system's default system "
+                 "output device back to device %d", inOutputDeviceID);
+
+        audioSystem.SetDefaultAudioDevice(false, true, inOutputDeviceID);
+    }
+}
+
+#pragma mark App Volumes
+
+void BGMBackgroundMusicDevice::SetAppVolume(SInt32 inVolume,
+                                            pid_t inAppProcessID,
+                                            CFStringRef inAppBundleID)
+{
+    BGMAssert((kAppRelativeVolumeMinRawValue <= inVolume) &&
+                      (inVolume <= kAppRelativeVolumeMaxRawValue),
+              "BGMBackgroundMusicDevice::SetAppVolume: Volume out of bounds");
+
+    // Clamp the volume to [kAppRelativeVolumeMinRawValue, kAppPanRightRawValue].
+    inVolume = std::max(kAppRelativeVolumeMinRawValue, inVolume);
+    inVolume = std::min(kAppRelativeVolumeMaxRawValue, inVolume);
+
+    SendAppVolumeOrPanToBGMDevice(inVolume,
+                                  CFSTR(kBGMAppVolumesKey_RelativeVolume),
+                                  inAppProcessID,
+                                  inAppBundleID);
+}
+
+void BGMBackgroundMusicDevice::SetAppPanPosition(SInt32 inPanPosition,
+                                                 pid_t inAppProcessID,
+                                                 CFStringRef inAppBundleID)
+{
+    BGMAssert((kAppPanLeftRawValue <= inPanPosition) && (inPanPosition <= kAppPanRightRawValue),
+              "BGMBackgroundMusicDevice::SetAppPanPosition: Pan position out of bounds");
+
+    // Clamp the pan position to [kAppPanLeftRawValue, kAppPanRightRawValue].
+    inPanPosition = std::max(kAppPanLeftRawValue, inPanPosition);
+    inPanPosition = std::min(kAppPanRightRawValue, inPanPosition);
+
+    SendAppVolumeOrPanToBGMDevice(inPanPosition,
+                                  CFSTR(kBGMAppVolumesKey_PanPosition),
+                                  inAppProcessID,
+                                  inAppBundleID);
+}
+
+void BGMBackgroundMusicDevice::SendAppVolumeOrPanToBGMDevice(SInt32 inNewValue,
+                                                             CFStringRef inVolumeTypeKey,
+                                                             pid_t inAppProcessID,
+                                                             CFStringRef inAppBundleID)
+{
+    CACFArray appVolumeChanges(true);
+
+    auto addVolumeChange = [&] (pid_t pid, CFStringRef bundleID)
+    {
+        CACFDictionary appVolumeChange(true);
+
+        appVolumeChange.AddSInt32(CFSTR(kBGMAppVolumesKey_ProcessID), pid);
+        appVolumeChange.AddString(CFSTR(kBGMAppVolumesKey_BundleID), bundleID);
+        appVolumeChange.AddSInt32(inVolumeTypeKey, inNewValue);
+
+        appVolumeChanges.AppendDictionary(appVolumeChange.GetDict());
+    };
+
+    addVolumeChange(inAppProcessID, inAppBundleID);
+
+    // Add the same change for each process the app is responsible for.
+    for(CFStringRef responsibleBundleID : ResponsibleBundleIDsOf(inAppBundleID))
+    {
+        // Send -1 as the PID so this volume will only ever be matched by bundle ID.
+        addVolumeChange(-1, responsibleBundleID);
+    }
+
+    CFPropertyListRef changesPList = appVolumeChanges.AsPropertyList();
+
+    // Send the change to BGMDevice.
+    SetPropertyData_CFType(kBGMAppVolumesAddress, changesPList);
+
+    // Also send it to the instance of BGMDevice that handles UI sounds.
+    mUISoundsBGMDevice.SetPropertyData_CFType(kBGMAppVolumesAddress, changesPList);
+}
+
+// This is a temporary solution that lets us control the volumes of some multiprocess apps, i.e.
+// apps that play their audio from a process with a different bundle ID.
+//
+// We can't just check the child processes of the apps' main processes because they're usually
+// created with launchd rather than being actual child processes. There's a private API to get the
+// processes that an app is "responsible for", so we'll try to use it in the proper fix and only use
+// this list if the API doesn't work.
+//
+// static
+std::vector<CFStringRef>
+BGMBackgroundMusicDevice::ResponsibleBundleIDsOf(CFStringRef inParentBundleID)
+{
+    std::map<CFStringRef, std::vector<CFStringRef>> bundleIDMap = {
+            // Finder
+            { CFSTR("com.apple.finder"), { CFSTR("com.apple.quicklook.ui.helper") } },
+            // Safari
+            { CFSTR("com.apple.Safari"), { CFSTR("com.apple.WebKit.WebContent") } },
+            // Firefox
+            { CFSTR("org.mozilla.firefox"), { CFSTR("org.mozilla.plugincontainer") } },
+            // Firefox Nightly
+            { CFSTR("org.mozilla.nightly"), { CFSTR("org.mozilla.plugincontainer") } },
+            // VMWare Fusion
+            { CFSTR("com.vmware.fusion"), { CFSTR("com.vmware.vmware-vmx") } },
+            // Parallels
+            { CFSTR("com.parallels.desktop.console"), { CFSTR("com.parallels.vm") } },
+            // MPlayer OSX Extended
+            { CFSTR("hu.mplayerhq.mplayerosx.extended"),
+                    { CFSTR("ch.sttz.mplayerosx.extended.binaries.officialsvn") } }
+    };
+
+    // Parallels' VM "dock helper" apps have bundle IDs like
+    // com.parallels.winapp.87f6bfc236d64d70a81c47f6243add4c.f5a25fdede514f7aa0a475a1873d3287.fs
+    if(CFStringHasPrefix(inParentBundleID, CFSTR("com.parallels.winapp.")))
+    {
+        return { CFSTR("com.parallels.vm") };
+    }
+
+    return bundleIDMap[inParentBundleID];
+}
+
+#pragma clang assume_nonnull end
+

BGMApp/BGMApp/BGMBackgroundMusicDevice.h
@@ -1,0 +1,132 @@
+// This file is part of Background Music.
+//
+// Background Music is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 2 of the
+// License, or (at your option) any later version.
+//
+// Background Music is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Background Music. If not, see <http://www.gnu.org/licenses/>.
+
+//
+//  BGMBackgroundMusicDevice.h
+//  BGMApp
+//
+//  Copyright © 2017 Kyle Neideck
+//
+//  The interface to BGMDevice, the main virtual device published by BGMDriver, and the second
+//  instance of that device, which handles UI-related audio. In most cases, users of this class
+//  should be able to think of it as representing a single device.
+//
+//  BGMDevice is the device that appears as "Background Music" in programs that list the output
+//  devices, e.g. System Preferences. It receives the system's audio, processes it and sends it to
+//  BGMApp by publishing an input stream. BGMApp then plays the audio on the user's real output
+//  device.
+//
+//  See BGMDriver/BGMDriver/BGM_Device.h.
+//
+
+#ifndef BGMApp__BGMBackgroundMusicDevice
+#define BGMApp__BGMBackgroundMusicDevice
+
+// Superclass Includes
+#include "BGMAudioDevice.h"
+
+// STL Includes
+#include <vector>
+
+
+#pragma clang assume_nonnull begin
+
+class BGMBackgroundMusicDevice
+:
+    public BGMAudioDevice
+{
+
+#pragma mark Construction/Destruction
+
+public:
+    /*!
+     @throws CAException If BGMDevice is not found or the HAL returns an error when queried for
+                         BGMDevice's current Audio Object ID.
+     */
+                        BGMBackgroundMusicDevice();
+    virtual            ~BGMBackgroundMusicDevice();
+
+#pragma mark Systemwide Default Device
+
+    /*!
+     Set BGMDevice as the default audio device for all processes.
+
+     @throws CAException If the HAL responds with an error.
+     */
+    void                SetAsOSDefault();
+    /*!
+     Replace BGMDevice as the default device with the output device.
+
+     @throws CAException If the HAL responds with an error.
+     */
+    void                UnsetAsOSDefault(AudioDeviceID inOutputDeviceID);
+
+#pragma mark App Volumes
+
+    /*!
+     @param inVolume A value between kAppRelativeVolumeMinRawValue and kAppRelativeVolumeMaxRawValue
+                     from BGM_Types.h. See kBGMAppVolumesKey_RelativeVolume in BGM_Types.h.
+     @param inAppProcessID The ID of app's main process (or the process it uses to play audio, if
+                           you've managed to figure that out). If an app has multiple audio
+                           processes, you can just set the volume for each of them. Pass -1 to omit
+                           this param.
+     @param inAppBundleID The app's bundle ID. Pass null to omit this param.
+     @throws CAException If the HAL returns an error when this function sends the volume change to
+                         BGMDevice.
+     */
+    void                SetAppVolume(SInt32 inVolume,
+                                     pid_t inAppProcessID,
+                                     CFStringRef inAppBundleID);
+    /*!
+     @param inPanPosition A value between kAppPanLeftRawValue and kAppPanRightRawValue from
+                          BGM_Types.h. A negative value has a higher proportion of left channel, and
+                          a positive value has a higher proportion of right channel.
+     @param inAppProcessID The ID of app's main process (or the process it uses to play audio, if
+                           you've managed to figure that out). If an app has multiple audio
+                           processes, you can just set the pan position for each of them. Pass -1 to
+                           omit this param.
+     @param inAppBundleID The app's bundle ID. Pass null to omit this param.
+     @throws CAException If the HAL returns an error when this function sends the pan position
+                         change to BGMDevice.
+     */
+    void                SetAppPanPosition(SInt32 inPanPosition,
+                                          pid_t inAppProcessID,
+                                          CFStringRef inAppBundleID);
+
+private:
+    void                SendAppVolumeOrPanToBGMDevice(SInt32 inNewValue,
+                                                      CFStringRef inVolumeTypeKey,
+                                                      pid_t inAppProcessID,
+                                                      CFStringRef inAppBundleID);
+
+    static std::vector<CFStringRef>
+                        ResponsibleBundleIDsOf(CFStringRef inParentBundleID);
+
+#pragma mark UI Sounds Instance
+
+public:
+    /*! @return The instance of BGMDevice that handles UI sounds. */
+    BGMAudioDevice      GetUISoundsBGMDeviceInstance() const { return mUISoundsBGMDevice; }
+
+private:
+    /*! The instance of BGMDevice that handles UI sounds. */
+    BGMAudioDevice      mUISoundsBGMDevice;
+
+};
+
+#pragma clang assume_nonnull end
+
+#endif /* BGMApp__BGMBackgroundMusicDevice */
+

commit 283db29fb448bf65eb908228609231fa76f5b386

App Volumes: Fix the hardcoded HAL client process bundle IDs.
Fix the workaround for apps whose bundle IDs don't match their CoreAudio
clients. BGMBackgroundMusicDevice::ResponsibleBundleIDsOf was always
returning an empty list.

Also fix over-releasing the app's bundle ID CFString in
BGMAVM_VolumeSlider::appVolumeChanged and
BGMAVM_PanSlider::appPanPositionChanged.

Both bugs were introduced two commits ago in
e05acde3514465e7b13125cb755b93cafaec2a3e.

BGMApp/BGMApp/BGMAppVolumes.mm
@@ -410,7 +410,7 @@ static CGFloat const kAppVolumeViewInitialHeight = 20;
     // [kAppRelativeVolumeMinRawValue, kAppRelativeVolumeMaxRawValue] already.
     context.audioDevices.bgmDevice.SetAppVolume(self.intValue,
                                                 appProcessID,
-                                                (__bridge CFStringRef)appBundleID);
+                                                (__bridge_retained CFStringRef)appBundleID);
 }
 
 @end
@@ -456,7 +456,7 @@ static CGFloat const kAppVolumeViewInitialHeight = 20;
     // The values from our sliders are in [kAppPanLeftRawValue, kAppPanRightRawValue] already.
     context.audioDevices.bgmDevice.SetAppPanPosition(self.intValue,
                                                      appProcessID,
-                                                     (__bridge CFStringRef)appBundleID);
+                                                     (__bridge_retained CFStringRef)appBundleID);
 }
 
 @end

BGMApp/BGMApp/BGMBackgroundMusicDevice.cpp
@@ -185,10 +185,10 @@ void BGMBackgroundMusicDevice::SendAppVolumeOrPanToBGMDevice(SInt32 inNewValue,
     addVolumeChange(inAppProcessID, inAppBundleID);
 
     // Add the same change for each process the app is responsible for.
-    for(CFStringRef responsibleBundleID : ResponsibleBundleIDsOf(inAppBundleID))
+    for(CACFString responsibleBundleID : ResponsibleBundleIDsOf(CACFString(inAppBundleID)))
     {
         // Send -1 as the PID so this volume will only ever be matched by bundle ID.
-        addVolumeChange(-1, responsibleBundleID);
+        addVolumeChange(-1, responsibleBundleID.GetCFString());
     }
 
     CFPropertyListRef changesPList = appVolumeChanges.AsPropertyList();
@@ -209,32 +209,32 @@ void BGMBackgroundMusicDevice::SendAppVolumeOrPanToBGMDevice(SInt32 inNewValue,
 // this list if the API doesn't work.
 //
 // static
-std::vector<CFStringRef>
-BGMBackgroundMusicDevice::ResponsibleBundleIDsOf(CFStringRef inParentBundleID)
+std::vector<CACFString>
+BGMBackgroundMusicDevice::ResponsibleBundleIDsOf(CACFString inParentBundleID)
 {
-    std::map<CFStringRef, std::vector<CFStringRef>> bundleIDMap = {
+    std::map<CACFString, std::vector<CACFString>> bundleIDMap = {
             // Finder
-            { CFSTR("com.apple.finder"), { CFSTR("com.apple.quicklook.ui.helper") } },
+            { "com.apple.finder", { "com.apple.quicklook.ui.helper" } },
             // Safari
-            { CFSTR("com.apple.Safari"), { CFSTR("com.apple.WebKit.WebContent") } },
+            { "com.apple.Safari", { "com.apple.WebKit.WebContent" } },
             // Firefox
-            { CFSTR("org.mozilla.firefox"), { CFSTR("org.mozilla.plugincontainer") } },
+            { "org.mozilla.firefox", { "org.mozilla.plugincontainer" } },
             // Firefox Nightly
-            { CFSTR("org.mozilla.nightly"), { CFSTR("org.mozilla.plugincontainer") } },
+            { "org.mozilla.nightly", { "org.mozilla.plugincontainer" } },
             // VMWare Fusion
-            { CFSTR("com.vmware.fusion"), { CFSTR("com.vmware.vmware-vmx") } },
+            { "com.vmware.fusion", { "com.vmware.vmware-vmx" } },
             // Parallels
-            { CFSTR("com.parallels.desktop.console"), { CFSTR("com.parallels.vm") } },
+            { "com.parallels.desktop.console", { "com.parallels.vm" } },
             // MPlayer OSX Extended
-            { CFSTR("hu.mplayerhq.mplayerosx.extended"),
-                    { CFSTR("ch.sttz.mplayerosx.extended.binaries.officialsvn") } }
+            { "hu.mplayerhq.mplayerosx.extended",
+                    { "ch.sttz.mplayerosx.extended.binaries.officialsvn" } }
     };
 
     // Parallels' VM "dock helper" apps have bundle IDs like
     // com.parallels.winapp.87f6bfc236d64d70a81c47f6243add4c.f5a25fdede514f7aa0a475a1873d3287.fs
-    if(CFStringHasPrefix(inParentBundleID, CFSTR("com.parallels.winapp.")))
+    if(inParentBundleID.StartsWith(CFSTR("com.parallels.winapp.")))
     {
-        return { CFSTR("com.parallels.vm") };
+        return { "com.parallels.vm" };
     }
 
     return bundleIDMap[inParentBundleID];

BGMApp/BGMApp/BGMBackgroundMusicDevice.h
@@ -40,6 +40,9 @@
 // Local Includes
 #include "BGM_Types.h"
 
+// PublicUtility Includes
+#include "CACFString.h"
+
 // STL Includes
 #include <vector>
 
@@ -123,8 +126,8 @@ private:
                                                       pid_t inAppProcessID,
                                                       CFStringRef inAppBundleID);
 
-    static std::vector<CFStringRef>
-                        ResponsibleBundleIDsOf(CFStringRef inParentBundleID);
+    static std::vector<CACFString>
+                        ResponsibleBundleIDsOf(CACFString inParentBundleID);
 
 #pragma mark Audible State
 