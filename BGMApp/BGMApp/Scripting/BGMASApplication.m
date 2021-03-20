c303f6034344d71a76fc1d07fe799f656c966720
Marcus Wu <marcus.wu@gopassport.com>
2021-03-19 20:52:04 -0400

    Fix licensing text in new files

--- BGMApp/BGMApp/Scripting/BGMASApplication.m
+++ BGMApp/BGMApp/Scripting/BGMASApplication.m
@@ -1,9 +1,24 @@
+// This file is part of Background Music.
 //
-//  BGMASApplication.m
-//  Background Music
+// Background Music is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 2 of the
+// License, or (at your option) any later version.
 //
+// Background Music is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Background Music. If not, see <http://www.gnu.org/licenses/>.
+
+//
+//  BGMASApplication.mm
+//  BGMApp
+//
+//  Copyright © 2017 Kyle Neideck
 //  Created by Marcus Wu on 3/17/21.
-//  Copyright © 2021 Background Music contributors. All rights reserved.
 //
 
 // Self Include
